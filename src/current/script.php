7950a74ee1ec7109ad103d3cea95aa5ef2e988bd
sdi68 <36002964+sdi68@users.noreply.github.com>
2023-01-29 19:37:24 +0300

    reformat code and add copyright

---
+++ src/current/script.php
@@ -0,0 +1,88 @@
+<?php defined('_JEXEC') or die;
+
+/**
+ * @package         Econsult Labs Library
+ * @version         1.0.0
+ *
+ * @author          ECL <info@econsultlab.ru>
+ * @link            https://econsultlab.ru
+ * @copyright       Copyright © 2023 ECL All Rights Reserved
+ * @license         http://www.gnu.org/licenses/gpl-2.0.html GNU/GPL
+ */
+
+use Joomla\Filesystem\Folder;
+use Joomla\Filesystem\Path;
+
+/**
+ * Class ECLabsInstallerScript
+ * @since 1.0.0
+ */
+class ECLabsInstallerScript
+{
+
+	public function postflight($type, $parent)
+	{
+
+		if ($type === 'install' || $type === 'update')
+		{
+			$this->copyMedia($parent->getParent());
+		}
+
+		if ($type === 'uninstall')
+		{
+			$this->deleteMedia();
+		}
+
+		return true;
+	}
+
+
+	protected function copyMedia($installer)
+	{
+		$dest    = JPATH_ROOT . '/media/eclabs';
+		$path    = Path::clean(JPATH_ROOT . '/libraries/eclabs/fields');
+		$folders = Folder::folders($path);
+
+		$copyFiles = [];
+
+		if (!file_exists($dest))
+		{
+			Folder::create($dest);
+		}
+
+		foreach ($folders as $folder)
+		{
+			$path_current = $path . '/' . $folder . '/media';
+			if (file_exists($path_current))
+			{
+				$copyFiles[] = [
+					'src'  => $path_current,
+					'dest' => $dest . '/' . $folder,
+					'type' => 'folder'
+				];
+			}
+		}
+
+		return $installer->copyFiles($copyFiles, true);
+	}
+
+
+	protected function deleteMedia()
+	{
+		$dest = JPATH_ROOT . '/media/eclabs';
+
+		if (file_exists($dest))
+		{
+			try
+			{
+				return Folder::delete($dest);
+			}
+			catch (Exception $e)
+			{
+				return false;
+			}
+		}
+
+		return true;
+	}
+}
