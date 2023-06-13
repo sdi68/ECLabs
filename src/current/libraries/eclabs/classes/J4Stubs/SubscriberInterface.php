f4ee01a68de2650f56f15f727699387ff9e6ebc4
sdi68 <36002964+sdi68@users.noreply.github.com>
2023-06-13 11:51:11 +0300

    Выпуск версии 1.0.5. Добавлена заглушка интерфейса SubscriberInterface Joomla4 для использования в Joomla 3.

---
+++ src/current/libraries/eclabs/classes/J4Stubs/SubscriberInterface.php
@@ -0,0 +1,21 @@
+<?php
+/**
+ * @package        Econsult Labs Library
+ * @version          1.0.11
+ * @author           ECL <info@econsultlab.ru>
+ * @link                https://econsultlab.ru
+ * @copyright      Copyright © 2023 ECL All Rights Reserved
+ * @license           http://www.gnu.org/licenses/gpl-2.0.html GNU/GPL
+ */
+
+namespace ECLabs\Library;
+
+/**
+ * Интерфейс - заглушка для использования в Joomla 3
+ * Используется только в Joomla 4
+ * @since 1.0.4
+ */
+interface SubscriberInterface
+{
+
+}
