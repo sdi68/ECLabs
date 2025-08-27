849496b476695e693a08ac1515777c73ea7b8e66
Дмитрий Санаев <dsanaev@mail.ru>
2025-08-27 18:13:46 +0300

    Корректировка поля для Joomla 5.

--- src/current/libraries/eclabs/src/Form/Field/CreatedByField.php
+++ src/current/libraries/eclabs/src/Form/Field/CreatedByField.php
@@ -6,6 +6,7 @@ namespace ECLabs\Library\Form\Field;
 
 use Joomla\CMS\Factory;
 use Joomla\CMS\Form\FormField;
+use Joomla\CMS\User\User;
 use Joomla\Database\DatabaseInterface;
 
 /**
@@ -13,13 +14,13 @@ use Joomla\Database\DatabaseInterface;
  */
 class CreatedByField extends FormField
 {
-	/**
-	 * The form field type
-	 *
-	 * @var		string
-	 * @since	1.6
-	 */
-	protected $type = 'createdby';
+    /**
+     * The form field type
+     *
+     * @var        string
+     * @since    1.6
+     */
+    protected $type = 'createdby';
 
     /**
      * Method to get the field input markup
@@ -28,54 +29,49 @@ class CreatedByField extends FormField
      * @throws \Exception
      * @since    1.6
      */
-	protected function getInput(): string
+    protected function getInput(): string
     {
-		// Get the current user
-		$user = Factory::getApplication()->getIdentity();
+        // Get the current user
+        $user = Factory::getApplication()->getIdentity();
 
-		// Set this to be sure the user texts are displayed as default
-		$userExists = true;
+        // Set this to be sure the user texts are displayed as default
+        $userExists = true;
 
-		// If the value is set
-		if ($this->value)
-		{
-			// Look for the user in the DB
-			$db = Factory::getContainer()->get(DatabaseInterface::class);
-			$query = $db->getQuery(true)
-				->select('id')
-				->from('#__users')
-				->where($db->qn('id') . ' = ' . $db->q($this->value));
-			$db->setQuery($query);
-			$userId = $db->loadResult();
+        // If the value is set
+        if ($this->value) {
+            // Look for the user in the DB
+            $db = Factory::getContainer()->get(DatabaseInterface::class);
+            $query = $db->getQuery(true)
+                ->select('id')
+                ->from('#__users')
+                ->where($db->qn('id') . ' = ' . $db->q($this->value));
+            $db->setQuery($query);
+            $userId = $db->loadResult();
 
-			// If the user exists in the DB
-			if ($userId)
-			{
-				// Get the user from the value in the input box
-				$user = Factory::getApplication()->loadIdentity($this->value);
-			}
-			else
-			{
-				$userExists = false;
+            // If the user exists in the DB
+            if ($userId) {
+                // Get the user from the value in the input box
+                //$user = Factory::getApplication()->loadIdentity($this->value);
+                $user = new User($userId);
+                Factory::getApplication()->loadIdentity($user);
+            } else {
+                $userExists = false;
 
-				// Otherwise set the value to the current user
-				$this->value = $user->id;
-			}
-		}
-		else
-		{
-			// Set the value to the current user
-			$this->value = $user->id;
-		}
+                // Otherwise set the value to the current user
+                $this->value = $user->id;
+            }
+        } else {
+            // Set the value to the current user
+            $this->value = $user->id;
+        }
 
-		// If the user ID exists in the DB (this user exists)
-		if ($userExists)
-		{
-			$html = $user->name . " (" . $user->username . ")";
-		}
+        // If the user ID exists in the DB (this user exists)
+        if ($userExists) {
+            $html = $user->name . " (" . $user->username . ")";
+        }
 
-		$html .= '<input type="hidden" name="' . $this->name . '" value="' . $this->value . '" />';
+        $html .= '<input type="hidden" name="' . $this->name . '" value="' . $this->value . '" />';
 
-		return $html;
-	}
+        return $html;
+    }
 }
