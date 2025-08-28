96fbffbc8d557496f81f1b1b64bc647735b731b8
Дмитрий Санаев <dsanaev@mail.ru>
2025-08-28 13:14:59 +0300

    Добавлен phpDocs.

--- src/current/libraries/eclabs/src/Traits/ECLLogTrait.php
+++ src/current/libraries/eclabs/src/Traits/ECLLogTrait.php
@@ -1,42 +1,66 @@
 <?php
+/**
+ * @package             Econsult Labs Library
+ * @version             __DEPLOYMENT_VERSION__
+ * @author              ECL <info@econsultlab.ru>
+ * @link                https://econsultlab.ru
+ * @copyright           Copyright © 2025 ECL All Rights Reserved
+ * @license             http://www.gnu.org/licenses/gpl-2.0.html GNU/GPL
+ */
 
 namespace ECLabs\Library\Traits;
+\defined('_JEXEC') or die;
 
 use ECLabs\Library\ECLLogging\ECLLogging;
 
+/**
+ * Трайт логирования. Формирует лог сущности в отдельном файле.
+ * @since 2.0.0
+ */
 trait ECLLogTrait
 {
-    protected string $_entity_name="";
+	/**
+	 * Имя сущности, которая создает файл лога.
+	 * @var string
+	 * @since 2.0.0
+	 */
+	protected string $_entity_name = "";
 
-    protected bool $_enable_logging;
-    /**
-     * Логирование работы плагина
-     *
-     * @param   string  $type     Тип записи
-     * @param   string  $message  Сообщение для записи в лог (например имя переменной)
-     * @param   array   $data     Переменная для записи в лог
-     *
-     * @return void
-     * @see   ECLLOG
-     * @since 1.0.20
-     */
-    protected final function _addLog(string $type, string $message, mixed $data = null): void
-    {
-        ECLLogging::add(
-            array(
-                "source"           => $this->_entity_name,
-                "enabled"          => $this->_enable_logging,
-                "logger"           => "ECLabs\\Library\\ECLLogging\\Loggers\\ECLabDefaultLogger",
-                "back_trace_level" => 3
-            ),
-            array(
-                "timestamp" => "",
-                "type"      => $type,
-                "caller"    => "",
-                "message"   => $message,
-                "data"      => $data,
-            )
-        );
-    }
+	/**
+	 * Флаг разрешения ведения лога.
+	 * @var bool
+	 * @since 2.0.0
+	 */
+	protected bool $_enable_logging;
+
+	/**
+	 * Логирование работы плагина
+	 *
+	 * @param   string  $type     Тип записи
+	 * @param   string  $message  Сообщение для записи в лог (например имя переменной)
+	 * @param   array   $data     Переменная для записи в лог
+	 *
+	 * @return void
+	 * @see   ECLLOG
+	 * @since 2.0.0
+	 */
+	protected final function _addLog(string $type, string $message, mixed $data = null): void
+	{
+		ECLLogging::add(
+			array(
+				"source"           => $this->_entity_name,
+				"enabled"          => $this->_enable_logging,
+				"logger"           => "ECLabs\\Library\\ECLLogging\\Loggers\\ECLabDefaultLogger",
+				"back_trace_level" => 3
+			),
+			array(
+				"timestamp" => "",
+				"type"      => $type,
+				"caller"    => "",
+				"message"   => $message,
+				"data"      => $data,
+			)
+		);
+	}
 
 }
