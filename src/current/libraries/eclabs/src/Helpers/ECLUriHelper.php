28f5c473668b522d224c088af3fe06e5b2abd288
Дмитрий Санаев <dsanaev@mail.ru>
2025-12-16 20:17:57 +0300

    Добавлен хелпер.

---
+++ src/current/libraries/eclabs/src/Helpers/ECLUriHelper.php
@@ -0,0 +1,61 @@
+<?php
+/**
+ * @package             Econsult Labs Library
+ * @version             __DEPLOYMENT_VERSION__
+ * @author              ECL <info@econsultlab.ru>
+ * @link                https://econsultlab.ru
+ * @copyright           Copyright © 2025 ECL All Rights Reserved
+ * @license             http://www.gnu.org/licenses/gpl-2.0.html GNU/GPL
+ */
+
+namespace ECLabs\Library\Helpers;
+
+use Joomla\CMS\Uri\Uri;
+use Joomla\Uri\UriHelper;
+
+class ECLUriHelper extends UriHelper
+{
+	/**
+	 * Удаляет GET параметр (массив параметров) из адреса
+	 *
+	 * @param   string        $url        Исходный url
+	 * @param   string|array  $get_param  Наименование параметра или массив GET параметров
+	 *
+	 * @return string
+	 * @since __DEPLOYMENT_VERSION__
+	 */
+	public static function removeGetParam(string $url, string|array $get_param): string
+	{
+
+		$uri = Uri::getInstance($url);
+		// Получаем все GET‑параметры
+		$queryParams = $uri->getQuery(true);
+		if (is_array($queryParams))
+		{
+			if (is_array($get_param))
+			{
+				foreach ($get_param as $v)
+				{
+					if (isset($queryParams[$v]))
+					{
+						// Удаляем нужный параметр
+						unset($queryParams[$v]);
+					}
+				}
+			}
+			else
+			{
+				if (isset($queryParams[$get_param]))
+				{
+					// Удаляем нужный параметр
+					unset($queryParams[$get_param]);
+				}
+			}
+			// Обновляем query-часть URL
+			$uri->setQuery($queryParams);
+		}
+
+		// Возвращаем полный URL без параметра
+		return $uri->toString();
+	}
+}
